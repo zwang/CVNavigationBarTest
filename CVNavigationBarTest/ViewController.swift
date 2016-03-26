1276b91e217f8195b03e7f9a83740c00bd07880b
Zhao Wang <zhao@playchat.net>
2016-03-25 23:48:16 -0700

    Initial Commit

---
+++ CVNavigationBarTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CVNavigationBarTest
+//
+//  Created by Zhao Wang on 3/25/16.
+//  Copyright © 2016 Zhao Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
